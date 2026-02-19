290f0b9a6824a4b4a9d57ac19a57ae004af9c12d
Martin Desombre <martin.desombre1@gmail.com>
2026-02-19 23:00:45 +0100

    initialisation et copie du code

---
+++ src/randomwalkvf/randomwalkcode.py
@@ -0,0 +1,106 @@
+import random
+import pygame
+import argparse
+
+
+#choses auxquelles penser:
+#plusieurs particules de differentes couleurs, mettre en ligne de commande le nb de particules, pas de collisions
+#mettre une proba peut etre pour les deplacements selon chacune des directions
+#comparer les coordonnées des particules avec la taille de l'ecran pour l'adapter et voir tous les chemins
+#demander à l'utilisateur quelle particule il veut voir pour zoomer
+#mettre nombre d'etapes à faire dans la commande
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Simulate random walk for particles.")
+    parser.add_argument("--particules", type=int, required=False, default=1, help="Number of particles")
+    parser.add_argument("--steps", type=int, required=False, default=100, help="Number of steps until end of simulation")
+    args = parser.parse_args()
+    sim = Simulation(args.particules, args.steps)
+    sim.afficher_chemin()
+
+
+class Particule:
+    def __init__(self, position: tuple, color):
+        self.position = list(position)
+        self.color = color
+        self.path = [tuple(self.position)]
+    
+    def move(self):
+        direction = random.randint(0,3)
+        if direction == 0:
+            self.position[0] += 1
+        elif direction == 1:
+            self.position[0] -= 1
+        elif direction == 2:
+            self.position[1] += 1
+        else:
+            self.position[1] -= 1
+        self.path.append(tuple(self.position))
+    
+
+
+class Simulation(): 
+    def __init__(self, nb_particules: int, steps: int):
+        self.particules = []
+        for i in range(nb_particules):
+            c = pygame.Color(0, 0, 0)
+            hue = (360 * i) / nb_particules       #on l'utilise pour avoir toujours des couleurs différentes
+            c.hsva = (hue, 100, 100, 100)
+            self.particules.append(Particule((0, 0), c))
+        
+        self.steps_totaux = steps
+
+    def afficher_chemin(self):
+        pygame.init()
+        pygame.font.init()
+        width, height = 800, 600
+        screen = pygame.display.set_mode((width, height))
+        running = True
+        current_step = 0
+        clock = pygame.time.Clock()
+        font = pygame.font.SysFont(None, 36)
+        print("Affichage lancé")
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+            if current_step < self.steps_totaux:
+                for particule in self.particules:
+                    particule.move()
+                current_step += 1
+            all_x = [p[0] for part in self.particules for p in part.path]
+            all_y = [p[1] for part in self.particules for p in part.path]
+            min_x, max_x = min(all_x), max(all_x)
+            min_y, max_y = min(all_y), max(all_y)
+            delta_x = max_x - min_x
+            delta_y = max_y - min_y
+            coef = 0.9
+            if delta_x!=0 and delta_y!=0:
+                scale = min((width * coef) / delta_x, (height * coef) / delta_y)    #on dezoome pour voir toutes les particules
+            else:
+                scale = 1
+
+        #il nous reste à adapter les coordonnées de chaque particule à la nouvelle échelle
+            def transform(x, y):
+                screen_x = int((x - (min_x + max_x) / 2) * scale + width / 2)
+                screen_y = int(-(y - (min_y + max_y) / 2) * scale + height / 2)
+                return (screen_x, screen_y)
+
+            screen.fill((0, 0, 0))
+            for particule in self.particules:
+                points = [transform(x, y) for x, y in particule.path]    #nouvelles coordonnées
+                if len(points) > 1:
+                    pygame.draw.lines(screen, particule.color, False, points, 2)
+            
+            text_surface = font.render(f"Step: {current_step} / {self.steps_totaux}",True,(255, 255, 255)) #on met a jour le nombre d'étapes
+            screen.blit(text_surface, (10, 10))
+            pygame.display.flip()
+             #5 images par seconde
+            clock.tick(5)
+        pygame.quit()
+
+
+
+
+#remarques: on a parfois l'impression que les particules ne se deplacent pas depuis le dernier endroit ou elles ont été
+# mais aussi depuis d'autres endroits sur leur chemin, c'est du au zoom auto
