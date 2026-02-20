bd4637b86b540dfa2fe0a8faada24df81b7862fa
Martin Desombre <martin.desombre1@gmail.com>
2026-02-20 11:13:29 +0100

    test fonction simulation chemin_sans_affichage

--- src/randomwalkvf/test_randomwalkcode.py
+++ src/randomwalkvf/test_randomwalkcode.py
@@ -10,3 +10,9 @@ def test_particule_move():  #pour verifier que la particule bouge et que son che
     x, y = p.position
     assert (abs(x) == 1 and y == 0) or (x == 0 and abs(y) == 1) #on verifie que la particule a bougé d'une seule case
 
+def test_simulation_chemin_sans_affichage():  #pour verifier que le nombre d'étapes est respecté
+    sim = Simulation(nb_particules=2, steps=5)
+    sim.chemin_sans_affichage()
+    for p in sim.particules:
+        assert len(p.path) == 6 #on verifie qu'il y a la position initiale+5 nouvelles positions
+
